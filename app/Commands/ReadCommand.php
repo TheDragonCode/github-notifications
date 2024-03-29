cf9967183015c10907a6817a3b7f0095f52c05bb
Andrey Helldar <helldar@dragon-code.pro>
2024-03-29 11:21:44 +0300

    Removed extra code

--- app/Commands/ReadCommand.php
+++ app/Commands/ReadCommand.php
@@ -17,7 +17,6 @@ class ReadCommand extends Command
         . ' {--i|without-issues : Exclude issues from processing}'
         . ' {--s|without-pulls : Exclude Pull Requests from processing}'
         . ' {--o|with-open : Process including open Issues and Pull Requests}'
-        . ' {--n|no-interaction : Do not ask any interactive question}'
         . ' {--token= : GitHub access token with "notifications" permissions}';
 
     protected $description = 'Read all issue notifications';
@@ -40,7 +39,7 @@ class ReadCommand extends Command
 
     protected function hasContinue(): bool
     {
-        return $this->noInteraction() || confirm('Continue');
+        return confirm('Continue');
     }
 
     protected function read(?string $repository): void
@@ -99,11 +98,6 @@ class ReadCommand extends Command
     protected function withOpen(): bool
     {
         return $this->option('with-open');
-    }
-
-    protected function noInteraction(): bool
-    {
-        return $this->option('no-interaction');
     }
 
     protected function token(): string
