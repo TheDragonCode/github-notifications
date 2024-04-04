1b8263d7337eecb4b8507d5ed91c8ad142e02da7
Andrey Helldar <helldar@dragon-code.pro>
2024-04-04 10:36:42 +0300

    Added ignoring mentions from Dependabot

--- app/Data/ItemData.php
+++ app/Data/ItemData.php
@@ -6,18 +6,19 @@ namespace DragonCode\GithubNotifications\Data;
 
 class ItemData extends Data
 {
-    public bool $isOpen;
+    public ?int $ownerId = null;
 
-    public bool $isMerged;
+    public bool $isOpen = false;
+
+    public bool $isMerged = true;
 
     public function __construct(?array $data)
     {
         if (is_null($data)) {
-            $this->isOpen   = false;
-            $this->isMerged = true;
-
             return;
         }
+
+        $this->ownerId = $this->get($data, 'user.id');
 
         $this->isOpen   = $this->get($data, 'state') === 'open';
         $this->isMerged = (bool) $this->get($data, 'merged');
